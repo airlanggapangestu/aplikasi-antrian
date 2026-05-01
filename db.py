439fb562a1dfac995f1580bdf3e2149eed3b5eea
Airlangga Pangestu <138566683+airlanggapangestu@users.noreply.github.com>
2026-04-30 18:49:21 -0900

    Refactor database connection into a function

--- db.py
+++ db.py
@@ -1,10 +1,9 @@
 import mysql.connector
 
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",
-    database="db_antrian"
-)
-
-cursor = db.cursor()
+def connect_db():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="antrian"
+    )
